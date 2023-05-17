9e985562a4046e09ad9b307767fcb1547ddd5089
Ben Richardson <benrichardson@Bens-MacBook-Air.local>
2023-05-17 13:05:38 -0500

    added greatest inc & dec

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,16 @@
 # import the os module and module for reading csv files
 import os
 import csv
+from datetime import datetime
+
 
 
 csvpath = os.path.join('Resources','budget_data.csv')
 
 total_months = 0
 net_profit = 0
-changes_list = []
-month_of_change_list = []
+revenue_changes = []
+date_and_revenue_changes = []
 previous = 0
 
 
@@ -26,11 +28,25 @@ with open(csvpath) as csvfile:
         net_profit = int(row[1]) + net_profit
         change = int(row[1]) - previous
         previous = int(row[1])
-        changes_list.append(change) 
+        revenue_changes.append(change)
 
-changes_list = changes_list[1:]
-average_change = sum(changes_list) / len(changes_list)
+        date_and_revenue_elements = [row[0],change]
+        date_and_revenue_changes.append(date_and_revenue_elements)
 
+
+revenue_changes = revenue_changes[1:]
+average_change = sum(revenue_changes) / len(revenue_changes)
+
+max_output = max(date_and_revenue_changes, key=lambda x: x[1])
+min_output = min(date_and_revenue_changes, key=lambda x: x[1])
+
+date_time_str = max_output[0] 
+date_time_obj = datetime.strptime(date_time_str, '%d-%b')
+max_date_new_format = (date_time_obj.strftime("%b-%d"))
+
+date_time_str = min_output[0]
+date_time_obj = datetime.strptime(date_time_str, '%d-%b')
+min_date_new_format = (date_time_obj.strftime("%b-%d"))
 
 print_statement = (
     f'Financial Analysis\n'
@@ -38,8 +54,12 @@ print_statement = (
     f'Total Months: ${total_months}\n'
     f'Total: {"$"}{net_profit}\n'
     f'Average Change: {"$"}{round((average_change),2)}\n'
+    f'Greatest Increase in Profits: {max_date_new_format} ({"$"}{max_output[1]})\n'
+    f'Greatest Decrease in Profits: {min_date_new_format} ({"$"}{min_output[1]})\n'
 )
 
 print(print_statement)
+
+
 
    
